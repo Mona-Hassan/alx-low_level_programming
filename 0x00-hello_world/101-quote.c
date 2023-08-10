293d71fdab92f3ab02d99221798f6da32e7ecd10
Mona-hassan <mona.abdulfattah.hassan@gmail.com>
2023-08-10 12:11:06 -0700

    s

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+include <stdio.h>
+
+/**
+ * main - print"and that piece of art is useful"
+ * Return: Always 0 (success)
+ */
+int mai(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
