2716e5061e8994930e386b1132dadd04b8288600
Mona-hassan <mona.abdulfattah.hassan@gmail.com>
2023-08-10 07:28:20 -0700

    s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
-/** 
- * main - prints "Programming is like building a multilingual puzzle
+/** main - prints "Programming is like building a multilingual puzzle
  * follwed by next line"
- *
  * Return: Always 0 (Success)
 */
-
- int main(void)
- {
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
- }
+}
