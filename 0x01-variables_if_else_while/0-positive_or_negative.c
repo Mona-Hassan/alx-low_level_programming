1b790ac4c0b0eb8185b56fede5a2080e644548e6
Mona-hassan <mona.abdulfattah.hassan@gmail.com>
2023-08-12 02:51:23 -0700

    s

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/* 
+ * main - generate " This program will assign a random number 
+ * to the variable n each time it is executed"
+ */
 int main(void)
 {
 	int n;
