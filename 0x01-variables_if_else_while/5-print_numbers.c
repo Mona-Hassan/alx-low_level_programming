064ce1d9e6965f7f22ef1e9927522a87176ceffb
Mona-hassan <mona.abdulfattah.hassan@gmail.com>
2023-08-12 10:46:38 -0700

    s

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * desc: all alphabet except q & e
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf('\n');
+	return (0);
+}
+
