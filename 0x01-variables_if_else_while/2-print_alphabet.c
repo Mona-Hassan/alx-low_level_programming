fdbf9d46ce2921930b4593cca0c54be07a8b2550
Mona-hassan <mona.abdulfattah.hassan@gmail.com>
2023-08-12 10:00:51 -0700

    b

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entr point
+ * desc: alaphabet in lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++
+	}
+	putchar("\n");
+	return (0);
+}
