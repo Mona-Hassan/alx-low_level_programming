b8335a6d588bf1f3cad016e38893f4378d0084b3
Mona-hassan <mona.abdulfattah.hassan@gmail.com>
2023-08-13 00:10:03 -0700

    s

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * desc: all single digit numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar(n + 48);
+	}
+	putchar('\n');
+	return (0);
+}
