30a9d572367f3acf21befe51e4d8a9c8fedcba90
Mona-hassan <mona.abdulfattah.hassan@gmail.com>
2023-08-13 00:17:51 -0700

    s

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * desc: the lowercase alphabet in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a = 122;
+
+	while (n >= 97)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
