bc0bfe77807b6222b7a7ac43cb1950a8156e43fe
Mona-hassan <mona.abdulfattah.hassan@gmail.com>
2023-08-15 07:28:03 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number
+ * @n: the num to be checked
+ * Returns 1 for positive num and -1 negative num
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar (48);
+		return (0);
+	}
+}
+
