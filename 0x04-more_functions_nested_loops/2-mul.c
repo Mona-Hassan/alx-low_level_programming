a467104cc8db0268ba7a0a545be26d8d9f2cbe2d
Mona-hassan <mona.abdulfattah.hassan@gmail.com>
2023-08-17 12:07:40 -0700

    s

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.c"
+
+/**
+ * mul - multiplies two integers
+ * @a: the first num to be checked
+ * @b: the second num to be checked
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
