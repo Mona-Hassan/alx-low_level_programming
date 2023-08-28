36ade62799741db82f0c1cd165e2eb1155706c72
Mona-hassan <mona.abdulfattah.hassan@gmail.com>
2023-08-28 14:23:45 -0700

    s

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - start point
+ * @s: the destination
+ * @b: byte constant
+ * @n: bytes
+ * Retur: 0
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int m;
+
+	for (m = 0; m < n; m++)
+	{
+		s[m] = b;
+	}
+	return (s);
+}
